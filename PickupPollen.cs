98d5335e1124e3ab8982331b9f309a7f1e5d074b
BrandonPerez655 <bmp25a@acu.edu>
2025-11-04 12:05:29 -0600

    Create PickupPollen.cs
    Added PickupPollen script for pollen collection

---
+++ PickupPollen.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class PickupPollen : MonoBehaviour
+{
+    private GameManager gameManager;
+    private bool didCountPickup = false;
+
+    private void Start()
+    {
+        // Finds the GameManager attached to your Canvas (or wherever it lives)
+        gameManager = GameObject.Find("Canvas").GetComponent<GameManager>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player") && !didCountPickup)
+        {
+            didCountPickup = true;
+            gameManager.numberOfPollen++; // Count pollen instead of keys
+            Destroy(gameObject); // Removs the pollen after collecting it
+        }
+    }
+}
+
